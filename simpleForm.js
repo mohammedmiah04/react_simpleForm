8cd28e3051da9370e07e0793a8fdc6765b92e0c8
mohammedmiah04 <54578668+mohammedmiah04@users.noreply.github.com>
2021-01-21 10:10:56 +0000

    Create simpleForm.js

---
+++ simpleForm.js
@@ -0,0 +1,47 @@
+
+import React, { useState } from "react";
+
+// const input = document.getElementById('myText');
+// const inputValue = input.value
+// React
+// value, onChange
+
+const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(firstName, email);
+  };
+
+  return (
+    <article>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="firstName">Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button type="Submit">Submit</button>
+      </form>
+    </article>
+  );
+};
+
+export default ControlledInputs;
